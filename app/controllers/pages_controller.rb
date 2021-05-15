29680c20ebf078d0648204511331e89bbbf6a194
Eunchong Kim <chariskimec@gmail.com>
2021-05-16 00:04:21 +0900

    Created get response to current view places and refresh heatmap

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,14 +3,46 @@ class PagesController < ApplicationController
     logger.info('hogehogehoge')
     places = get_places(35.6681625, 139.6007829, 'school')
     @place_lat_lng_arr = places.map{ |p| {lat: p['geometry']['location']['lat'], lng: p['geometry']['location']['lng']} }
-    logger.info(@place_lat_lng_arr)
+    #logger.info(@place_lat_lng_arr)
+  end
+
+  def get_places_json()
+    lat = params[:lat]
+    lng = params[:lng]
+    keyword = params[:keyword]
+    logger.info(lat)
+    logger.info(lng)
+    logger.info(keyword)
+    uri = URI("https://maps.googleapis.com/maps/api/place/nearbysearch/json")
+    params = {
+      location: "#{lat},#{lng}",
+      radius: 1000, # in m
+      keyword: keyword,
+      key: Rails.application.credentials.GOOGLE_API_KEY
+    }
+    uri.query = URI.encode_www_form(params)
+
+    req = Net::HTTP::Post.new(uri.request_uri, 'Content-Type': 'application/json')
+
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    res = http.request(req)
+
+    if res.body.nil? || res.body == "[]"
+      return []
+    else
+      res_json = JSON.parse(res.body)
+      place_lat_lng_arr = res_json['results'].map{ |p| {lat: p['geometry']['location']['lat'], lng: p['geometry']['location']['lng']} }
+      render json: place_lat_lng_arr
+    end
   end
 
 
   private
 
     def get_places(lat, lng, keyword)
-      #uri = URI.parse("https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input="+keyword+"&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key="+Rails.application.credentials.GOOGLE_API_KEY)
       uri = URI("https://maps.googleapis.com/maps/api/place/nearbysearch/json")
       params = {
         location: "#{lat},#{lng}",
@@ -33,8 +65,6 @@ class PagesController < ApplicationController
         return []
       else
         res_json = JSON.parse(res.body)
-        logger.info(res_json)
-        logger.info(res_json['results'])
         return res_json['results']
       end
     end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
 
+  match '/get_places_json', to: 'pages#get_places_json', via: 'get'
+
   resources :posts
 end
